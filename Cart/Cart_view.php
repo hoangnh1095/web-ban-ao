92f187d8ac1a0b8d75fa95112c5c3490a64e15df
sonthaithanh <sonthaithanh99@gmail.com>
2021-05-20 15:55:13 +0700

    Cart
    Cart1

--- Cart/Cart_view.php
+++ Cart/Cart_view.php
@@ -4,8 +4,17 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Trang Cart</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-	123123123123
+	<div class="container">
+	  <h2>Giỏ Hàng </h2>    
+	  <hr>      
+	  <table class="table table-bordered">
+	  </table>
+	</div>
 </body>
 </html>
